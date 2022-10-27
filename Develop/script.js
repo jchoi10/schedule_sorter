d8802f80a9c430c5cfc47382cedcc2fcff723e3e
jchoi10 <noreply>
2022-10-27 07:18:53 -0400

    moment.js and jquery not working...

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,12 @@
 //Display calender
 var day = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").html(day);
+
+//save task to local storage
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var task = $(this).sibling(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, task);
+    })
+})
