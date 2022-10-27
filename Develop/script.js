cedf73b5ca4c3847701ba03051a151da68da501d
jchoi10 <noreply>
2022-10-27 19:21:58 -0400

    fixed local storage issue

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ $("#currentDay").append(day);
 //save task to local storage
 $(document).ready(function () {
     $(".saveBtn").on("click", function () {
-        var task = $(this).sibling(".description").val();
+        var task = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, task);
     })
