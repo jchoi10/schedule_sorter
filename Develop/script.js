d5e330363e094ba8b7eb04074ff1f96584b51d35
jchoi10 <noreply>
2022-10-27 19:03:22 -0400

    fixed jquery

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 //Display calender
 var day = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#currentDay").html(day);
+$("#currentDay").append(day);
 
 //save task to local storage
 $(document).ready(function () {
