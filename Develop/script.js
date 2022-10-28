ba05ae089e239d13409176637714c8a8b633ff39
jchoi10 <noreply>
2022-10-27 21:56:48 -0400

    get saved info from local storage js

--- Develop/script.js
+++ Develop/script.js
@@ -9,4 +9,22 @@ $(document).ready(function () {
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, task);
     })
+
+    $("#0800hour .description").val(localStorage.getItem("0800hour"));
+    $("#0900hour .description").val(localStorage.getItem("0900hour"));
+    $("#1000hour .description").val(localStorage.getItem("1000hour"));
+    $("#1100hour .description").val(localStorage.getItem("1100hour"));
+    $("#1200hour .description").val(localStorage.getItem("1200hour"));
+    $("#1300hour .description").val(localStorage.getItem("1300hour"));
+    $("#1400hour .description").val(localStorage.getItem("1400hour"));
+    $("#1500hour .description").val(localStorage.getItem("1500hour"));
+    $("#1600hour .description").val(localStorage.getItem("1600hour"));
+    $("#1700hour .description").val(localStorage.getItem("1700hour"));
+    $("#1800hour .description").val(localStorage.getItem("1800hour"));
+    $("#1900hour .description").val(localStorage.getItem("1900hour"));
+
+    function hourCounter () {
+        var currentTime= moment().hour();
+
+    }
 })
